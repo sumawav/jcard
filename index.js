b27a82cec9f0140fd7c75883cb18a6263c3c2b0b
Souma <mondal.souma@gmail.com>
2022-03-08 13:39:29 -0600

    window.log to enable logging. restricts crop window to image boundary

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import Cropper from './cropper.esm.js';
 const image = document.getElementById('image');
 const cropper = new Cropper(image, {
     // aspectRatio: 16 / 9,
+    viewMode: 1,
     crop(event) {
+        if (!window.log) {
+            return;
+        }
         console.log(event.detail.x);
         console.log(event.detail.y);
         console.log(event.detail.width);
