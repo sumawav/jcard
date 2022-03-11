8ac42a09e81146ea0b8dc21322de8b6dd73928bf
Souma <mondal.souma@gmail.com>
2022-03-10 22:17:15 -0600

    able to read in image file and reject non image

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 import Cropper from './cropper.esm.js';
+const URL = window.URL || window.webkitURL;
+const REGEXP_MIME_TYPE_IMAGES = /^image\/\w+$/;
+const REGEXP_URLS = /^(?:https?|data):/;
 
-const image = document.getElementById('image');
-const cropper = new Cropper(image, {
-    // aspectRatio: 16 / 9,
-    viewMode: 1,
-    crop(event) {
-        if (!window.log) {
+const fileInput = document.getElementById('file');
+
+function read(file, event) {
+    return new Promise((resolve, reject) => {
+        if (!file) {
+            resolve();
             return;
         }
-        console.log(event.detail.x);
-        console.log(event.detail.y);
-        console.log(event.detail.width);
-        console.log(event.detail.height);
-        // console.log(event.detail.rotate);
-        // console.log(event.detail.scaleX);
-        // console.log(event.detail.scaleY);
-    },
-});
+
+        if (REGEXP_MIME_TYPE_IMAGES.test(file.type)) {
+            if (URL) {
+                resolve({
+                    loaded: true,
+                    name: file.name,
+                    type: file.type,
+                    url: URL.createObjectURL(file),
+                });
+            } else {
+                reject(new Error('Your browser is not supported.'));
+            }
+        } else {
+            reject(
+                new Error(
+                    `Please ${event ? event.type : 'choose'} an image file.`
+                )
+            );
+        }
+    });
+}
+
+fileInput.onchange = ({ target }) => {
+    const { files } = target;
+
+    if (files && files.length > 0) {
+        read(files[0])
+            .then((data) => {
+                target.value = '';
+                console.log('successful read');
+                console.log(data);
+                // this.update(data);
+            })
+            .catch((e) => {
+                target.value = '';
+                this.alert(e);
+            });
+    }
+};
